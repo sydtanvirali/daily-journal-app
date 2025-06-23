54722a0d889e357815be95630d3ad899fc53fead
Tanvir Ali <tanvir@Tanvirs-MacBook-Air.local>
2025-06-23 21:02:50 +0530

    bug fix

--- client/src/store/api/authApi.ts
+++ client/src/store/api/authApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const authApi = createApi({
   reducerPath: "authApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: `${import.meta.env.VITE_API_URL}` || "http://localhost:8000/api",
+    baseUrl: `${import.meta.env.VITE_API_URL}`,
   }),
   tagTypes: ["User"],
   endpoints: (builder) => ({
